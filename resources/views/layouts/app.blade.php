4e8975a96def321d3e55e2c7546e9cb0b99c0b0f
Valifer13 <vianviszello@gmail.com>
2024-11-14 17:26:18 +0800

    edited the title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>@yield('title')</title>
+	<title>Kedai Kopi | @yield('title')</title>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 	<!-- Google Fonts -->
