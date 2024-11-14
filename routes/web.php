751283658a3b2ef9ab76b0afa9807ac71dc3f76c
Valifer13 <vianviszello@gmail.com>
2024-11-14 17:22:36 +0800

    create route and view for blog and contact page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,12 @@ Route::get('/reservation', function() {
     $menu = json_decode(file_get_contents(storage_path('data/data.json')), true);
     return view('pages.reservation', ['menu' => $menu]);
 })->name('reservation');
+
+Route::get('/blog', function() {
+    $data = json_decode(file_get_contents(storage_path('data/data.json')), true);
+    return view('pages.blog', ['articles' => $data['articles']]);
+})->name('blog');
+
+Route::get('/contact', function() {
+    return view('pages.contact');
+})->name('contact');
